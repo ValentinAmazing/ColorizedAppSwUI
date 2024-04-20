f13d882e3c6e3b3d9f7ca198376e985cb4c8faa4
ValentinAmazing <podrezva@bk.ru>
2024-04-20 22:17:44 +0700

    Initial Commit

---
+++ ColorizedAppSwUI/ColorizedAppSwUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedAppSwUIApp.swift
+//  ColorizedAppSwUI
+//
+//  Created by Valentin on 20.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedAppSwUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
